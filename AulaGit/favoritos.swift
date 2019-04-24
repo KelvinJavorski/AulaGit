c190d92b784a8c04ade8aea8cc26d969484de72a
Kelvin Javorski Soares <kelvin.javorskisoares@gmail.com>
2019-04-24 14:51:45 -0300

    adicionado arquivos

---
+++ AulaGit/favoritos.swift
@@ -0,0 +1,9 @@
+//
+//  favoritos.swift
+//  AulaGit
+//
+//  Created by Kelvin Javorski Soares on 24/04/19.
+//  Copyright © 2019 Kelvin Javorski Soares. All rights reserved.
+//
+
+import Foundation
